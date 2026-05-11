b97f4612e311304c98fe87da99f0b91648bf4b64
Le Trung Kien <kienltts01654@gmail.com>
2026-05-11 10:44:01 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Employee create(
+            @RequestBody Employee employee) {
+
+        return service.create(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee update(
+            @PathVariable Long id,
+            @RequestBody Employee employee) {
+
+        return service.update(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+
+        service.delete(id);
+
+        return "Deleted";
+    }
+}
