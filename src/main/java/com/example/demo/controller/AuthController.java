c801cf7745f85635811ae0f3de928859d7fe3815
Le Trung Kien <kienltts01654@gmail.com>
2026-05-11 11:52:34 +0700

    restore jpa

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,38 +1,38 @@
-//package com.example.demo.controller;
-//
-//import com.example.demo.dto.LoginRequest;
-//import com.example.demo.entity.User;
-//import com.example.demo.repository.UserRepository;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/auth")
-//public class AuthController {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @PostMapping("/login")
-//    public String login(
-//            @RequestBody LoginRequest request) {
-//
-//        User user = userRepository
-//                .findByUsername(request.getUsername())
-//                .orElse(null);
-//
-//        if(user == null) {
-//            return "User not found";
-//        }
-//
-//        if(user.getPassword()
-//                .equals(request.getPassword())) {
-//
-//            return "Login success. Role: "
-//                    + user.getRole();
-//        }
-//
-//        return "Wrong password";
-//    }
-//}
+package com.example.demo.controller;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public String login(
+            @RequestBody LoginRequest request) {
+
+        User user = userRepository
+                .findByUsername(request.getUsername())
+                .orElse(null);
+
+        if(user == null) {
+            return "User not found";
+        }
+
+        if(user.getPassword()
+                .equals(request.getPassword())) {
+
+            return "Login success. Role: "
+                    + user.getRole();
+        }
+
+        return "Wrong password";
+    }
+}
--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,35 +1,35 @@
-//package com.example.demo.service;
-//
-//import com.example.demo.entity.Employee;
-//import com.example.demo.repository.EmployeeRepository;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class EmployeeService {
-//
-//    @Autowired
-//    private EmployeeRepository repo;
-//
-//    public List<Employee> getAll() {
-//        return repo.findAll();
-//    }
-//
-//    public Employee create(Employee employee) {
-//        return repo.save(employee);
-//    }
-//
-//    public Employee update(Long id, Employee employee) {
-//
-//        employee.setId(id);
-//
-//        return repo.save(employee);
-//    }
-//
-//    public void delete(Long id) {
-//        repo.deleteById(id);
-//    }
-//}
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repo;
+
+    public List<Employee> getAll() {
+        return repo.findAll();
+    }
+
+    public Employee create(Employee employee) {
+        return repo.save(employee);
+    }
+
+    public Employee update(Long id, Employee employee) {
+
+        employee.setId(id);
+
+        return repo.save(employee);
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+}
