4d9dc5468337502ae7e3d63b4473974e1aa68f56
Le Trung Kien <kienltts01654@gmail.com>
2026-05-11 11:28:16 +0700

    add home route

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello Spring Boot";
+    public String home() {
+        return "Backend is running!";
     }
 }
